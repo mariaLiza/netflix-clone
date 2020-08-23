83a143a22691e2ee3664a45a2c3476b8d2d066df
maria liza <mariamartinez@pursuit.org>
2020-08-23 19:02:23 -0400

    fix bug in header

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
 import Row from "./Row";
-import requests from "./requests";
 import Banner from "./Banner";
+import requests from "./requests";
+import "./App.css";
 
 function App() {
   return (
--- netflix/src/Banner.js
+++ netflix/src/Banner.js
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "./axios";
 import requests from "./requests";
+
+// const baseUrl = "https://image.tmdb.org/t/p/original";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const request = await axios.get(requests.fetchNetflixOriginals);
-      debugger;
+    fetchHeader();
+  }, []);
+
+  const fetchHeader = async () => {
+    try {
+      let res = await axios.get(requests.fetchTrending);
+    //   debugger;
+      console.log(res);
       setMovie(
-        request.data.results[
-          Math.floor(Math.random() * request.data.results.length - 1)
+        res.data.results[
+          Math.floor(Math.random() * res.data.results.length - 1)
         ]
       );
-      return request;
-    };
-    fetchData();
-  }, []);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   console.log(movie);
 
